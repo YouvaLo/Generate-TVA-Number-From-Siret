1873f5df1c7e92b4c9eef56573855acdd16a3a9a
Youva LOUNAS <youva.lounas.97@gmail.com>
2024-11-03 23:09:32 +0100

    Add verify siret service

---
+++ VerifySiretGenerateTVA/services/VerifySiretNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using VerifySiretGenerateTVA.services;
+
+namespace VerifySiretGenerateTVA.Utils;
+
+public class VerifySiretNumber : IVerifySiretNumber
+{
+   // Luhn Algorithm to verify if Siret number is valid
+   public bool IsSiretValid(string siret)
+   {
+      if (string.IsNullOrEmpty(siret) || siret.Length != 14) return false;
+
+      int sum = 0;
+      bool temp = false;
+
+      for (int i = siret.Length - 1; i >= 0; i--)
+      {
+         int digit = int.Parse(siret[i].ToString());
+
+         if (temp)
+         {
+            digit *= 2;
+            if (digit > 9)
+            {
+               digit -= 9;
+            }
+         }
+
+         sum += digit;
+         temp = !temp;
+      }
+
+      return (sum % 10 == 0);
+   }
+}
